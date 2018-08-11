aa359b1b28017a60215b1546b9913ed3b14dab2d
Will Kearney <wkearn@gmail.com>
2018-08-11 14:31:28 -0400

    Add tests
    These just exercise the code

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,16 @@
-using ThompsonSampling
-@static if VERSION < v"0.7.0-DEV.2005"
-    using Base.Test
-else
-    using Test
-end
+include("thompson.jl")
 
-# write your own tests here
-@test 1 == 2
+x = linspace(0,2pi,100)
+y = linspace(0,2pi,100)
+
+g(x,y) = sin(x)*sin(y)
+
+gη(x) = g(x[1],x[2]) + 0.05*randn()
+
+x0 = [π 3π/2;π π+1.0]
+y0 = [gη(x0[:,i]) for i in 1:size(x0,2)]
+
+t = ThompsonSampler(gη,x0,y0,100,[0.0,0.0],[2pi,2pi])
+
+collect(Iterators.take(t,2))
+@time collect(Iterators.take(t,50));
